65eaca3d11e90a2731e0e6b9b21332b5c7dc8e1d
Sutiksh Verma <verma.sutiksh27@gmail.com>
2021-02-28 03:41:45 +0530

    Committing once again

---
+++ AR Building Plan/Assets/SceneHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class SceneHandler : MonoBehaviour
+{
+	public void ChangeScene()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+}
